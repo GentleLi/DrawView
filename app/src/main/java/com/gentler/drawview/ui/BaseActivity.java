ca7dada216e270cc90e477d267172434ed1a5b8e
lijiantao <lijiantao@021.com>
2017-11-09 15:57:32 +0800

    优化项目架构

--- app/src/main/java/com/gentler/drawview/ui/BaseActivity.java
+++ app/src/main/java/com/gentler/drawview/ui/BaseActivity.java
@@ -5,7 +5,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.gentler.drawview.impl.IActivityLifecycle;
-import com.gentler.drawview.utils.BarUtils;
 
 import butterknife.ButterKnife;
 
@@ -21,8 +20,7 @@ public abstract class BaseActivity extends AppCompatActivity implements IActivit
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mActivity=this;
-
-        BarUtils.hideStatusBar(mActivity);
+//        BarUtils.hideStatusBar(mActivity);
         setContentView(getLayoutId());
         ButterKnife.bind(this);
         initView();
